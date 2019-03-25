2788c81cb18f2244cb66842750c43cb3bb98843d
Joseph Scott <jbs198585@gmail.com>
2019-03-25 06:11:24 -0400

    second part solution

--- src/com/joescott/Main.java
+++ src/com/joescott/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945L));
     }
 
     private static String getDurationString(long minutes, long seconds) {
@@ -14,6 +15,17 @@ public class Main {
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
         return hours + "h " + remainingMinutes + "m " + seconds + "s";
+    }
+
+    private static String getDurationString(long seconds) {
+        if (seconds < 0) {
+            return "Invalid value";
+        }
+
+        long minutes = seconds / 60;
+        long remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes, remainingSeconds);
     }
 
 }
